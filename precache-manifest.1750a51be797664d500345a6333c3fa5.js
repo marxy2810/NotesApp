02824c2eb5c6b48965e2b8ff35fb671c8d8c3cab
Andrew Marks <andrewmarks@Andrews-Mac-Mini.local>
2021-02-14 22:17:23 +0000

    Updates

---
+++ precache-manifest.1750a51be797664d500345a6333c3fa5.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "73f82058dcc6e8d5d3f084cf6ee9df6b",
+    "url": "/NotesApp/index.html"
+  },
+  {
+    "revision": "2f26d36170f4425e7ea5",
+    "url": "/NotesApp/static/js/2.d3876aa1.chunk.js"
+  },
+  {
+    "revision": "a2e5604ac40d581e9577",
+    "url": "/NotesApp/static/js/main.860bfad0.chunk.js"
+  },
+  {
+    "revision": "37767e4bbb0ba7a38a5c",
+    "url": "/NotesApp/static/js/runtime-main.5e4f1e15.js"
+  }
+]);
